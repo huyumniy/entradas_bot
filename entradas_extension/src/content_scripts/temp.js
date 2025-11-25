0987b8efafa6769ff52c9356c887864ad1390a05
huyumniy <vladkg53@gmail.com>
2025-11-25 17:07:25 +0200

    cast images update

---
+++ entradas_extension/src/content_scripts/temp.js
@@ -0,0 +1,22 @@
+fetch("https://deportes.entradas.com/sports-web/userData/RMA/9559", {
+    "headers": {
+      "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+      "accept-language": "uk,en-US;q=0.9,en;q=0.8",
+      "cache-control": "no-cache",
+      "content-type": "application/x-www-form-urlencoded",
+      "pragma": "no-cache",
+      "priority": "u=0, i",
+      "sec-ch-ua": "\"Google Chrome\";v=\"129\", \"Not=A?Brand\";v=\"8\", \"Chromium\";v=\"129\"",
+      "sec-ch-ua-mobile": "?0",
+      "sec-ch-ua-platform": "\"macOS\"",
+      "sec-fetch-dest": "document",
+      "sec-fetch-mode": "navigate",
+      "sec-fetch-site": "same-origin",
+      "upgrade-insecure-requests": "1",
+      "cookie": "JSESSIONID=CB13257E991CAECCBC4874816F2C3514; org.springframework.web.servlet.i18n.CookieLocaleResolver.LOCALE=en; SP_REF=e4d23ff2-e2f6-4a72-a289-b4695f9ec416; QueueITAccepted-SDFrts345E-V3_rmlaligaecom=EventId%3Drmlaligaecom%26QueueId%3D0cf953cb-29b4-4e46-8241-b760263778f5%26RedirectType%3Dsafetynet%26IssueTime%3D1739897712%26Hash%3D33ebfcb7f613249b790a5d0038371c37d3fd240c96eb483d52862ed5a96e67b6",
+      "Referer": "https://deportes.entradas.com/sports-web/Real_Madrid/Futbol/2024-2025/LALIGA/REAL_MADRID_CF_-_GIRONA_FC/?&lang=en",
+      "Referrer-Policy": "strict-origin-when-cross-origin"
+    },
+    "body": "bookingLocator=11741712609&sessionId=9559&teamUcc=RMA&zoneId=26573&trackerBooking=%7B%22numTickets%22%3A%221%22%2C%22sectorData%22%3A%7B%22sectorName%22%3A%22Lateral+Este%22%2C%22availableSeats%22%3A%220%22%2C%22availableVipSeats%22%3A%221%22%2C%22minPrice%22%3A%22990%22%7D%2C%22zoneData%22%3A%7B%22zoneId%22%3A%22336%22%2C%22zoneName%22%3A%22VIP+336%22%2C%22sector%22%3A%22336%22%2C%22defaultPrice%22%3A%22990%22%2C%22abbreviatedZoneCode%22%3A%22%22%7D%2C%22vipZone%22%3Atrue%7D&submitDone=1&onlyVip=0&seatId=4867680%3A9559",
+    "method": "POST"
+  });
